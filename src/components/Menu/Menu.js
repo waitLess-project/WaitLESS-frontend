9b1e5349883ced8c374639807dbc30dff026b871
Jacob Lepler <lepler.jacob@gmail.com>
2021-06-12 19:13:59 -0400

    adding styling to search/filter element of menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,74 +1,78 @@
-import React, { useState, useEffect } from 'react'
-import MealCard from '../MealCard/MealCard'
-import './styles.css'
+import React, { useState, useEffect } from "react";
+import MealCard from "../MealCard/MealCard";
+import "./styles.css";
 
 const Menu = () => {
-    const [search, setSearch] = useState("")
-    const [filter, setFilter] = useState("")
-    const [meals, setMeals] = useState([])
-    const [order, setOrder] = useState([])
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
+  const [meals, setMeals] = useState([]);
+  const [order, setOrder] = useState([]);
 
-    //fetches the meals
-    const fetchMeals = () => {
-        fetch("http://localhost:3000/meals").then(res => res.json())
-            .then(setMeals)
+  //fetches the meals
+  const fetchMeals = () => {
+    fetch("http://localhost:3000/meals")
+      .then((res) => res.json())
+      .then(setMeals);
+  };
+
+  //to fetch the meals when this component is opened
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+
+  //adds a meal to the order
+  const addToOrder = (id) => {
+    if (!order.includes(id)) {
+      setOrder((order) => [...order, id]);
     }
+  };
 
-    //to fetch the meals when this component is opened
-    useEffect(() => {
-        fetchMeals()
-    }, [])
+  //gets the orders from the state
+  const getOrders = () => meals.filter((meal) => order.includes(meal.id));
 
-    //adds a meal to the order
-    const addToOrder = id => {
-        if (!order.includes(id)) {
-            setOrder(order => ([...order, id]))
-        }
-    }
-
-    //gets the orders from the state
-    const getOrders = () => meals.filter(meal => order.includes(meal.id))
-
-    return (
-        <div className="menu">
-            <div>
-                <form>
-                    <input
-                        type="text"
-                        label="searchBar"
-                        placeholder="enter an item"
-                        name="searchBar"
-                        value={search}
-                        onChange={e => setSearch(e.target.value)}
-                    />
-                    <input type="submit" />
-                </form>
-                <select onChange={e => setFilter(e.target.value)}>
-                    <option value="specials">Specials</option>
-                    <option value="breakfast">Breakfast</option>
-                    <option value="brunch">Brunch</option>
-                    <option value="dinner">Dinner</option>
-                </select>
-            </div>
-            <div className="menu-view">
-                <div className="meal-cards">
-                    {meals.map(meal => <MealCard meal={meal} key={meal.id} addToOrder={addToOrder} order={order}/>)}
-                </div>
-                <div className="cart">
-                    <h4>CART/ORDER</h4>
-                    <ul>
-                        {getOrders().map(order => 
-                            <li>
-                                {order.title}
-                            </li>
-                        )}
-                    </ul>
-
-                </div>
-            </div>
-
+  return (
+    <div className="menu">
+      <div className="search-filter">
+        <form>
+          <input
+            type="text"
+            label="searchBar"
+            placeholder="enter an item"
+            name="searchBar"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <input type="submit" />
+        </form>
+        <select onChange={(e) => setFilter(e.target.value)}>
+          <option value="specials">Specials</option>
+          <option value="breakfast">Breakfast</option>
+          <option value="brunch">Brunch</option>
+          <option value="dinner">Dinner</option>
+        </select>
+      </div>
+      <div className="menu-view">
+        <div className="meal-cards">
+          {meals.map((meal) => (
+            <MealCard
+              meal={meal}
+              key={meal.id}
+              addToOrder={addToOrder}
+              order={order}
+            />
+          ))}
         </div>
-    )
-}
+        <div className="cart">
+          <h4>CART/ORDER</h4>
+          <ul>
+            {getOrders().map((order) => (
+              <li>{order.title}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
