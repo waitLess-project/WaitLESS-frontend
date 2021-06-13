a0ad2c9abe99d093f248b5153ff149c0b376be5e
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-06-13 15:51:49 -0400

    Styling on Landing page, Footer, and checkout

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,20 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './styles.css'
 
+
 const LandingPage = () => {
     return (
         <div className="landingPage">
             <div>
-                <button className="waitless-big">
-                    WaitLESS
+                <button className="waitless-big logo-button">
+                    <img className="logo" src={"./waitless.png"} />
                 </button>
             </div>
             <div className="landingPage-options">
-                <button className="landingPage-btn">
+                <button className="landingPage-btn " >
                     Rewards
                 </button>
                 <Link to="/menu">
-                    <button className="landingPage-btn" style={{"cursor": "pointer"}}>
+                    <button className="landingPage-btn " style={{"cursor": "pointer"}}>
                     Get Started
                     </button>                
                 </Link>
