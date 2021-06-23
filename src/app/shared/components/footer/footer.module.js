c35dc1d6eac90b853c504394973ba20acb7273a4
Jacob Lepler <lepler.jacob@gmail.com>
2021-06-23 18:13:16 -0400

    changed function footer component to class

--- src/app/shared/components/footer/footer.module.js
+++ src/app/shared/components/footer/footer.module.js
@@ -1,20 +1,15 @@
-import React from 'react'
-import './styles.css'
+import React, { Component } from "react";
+import "./footer.styles.css";
 
-const Footer = () => {
+export default class Footer extends Component {
+    
+  render() {
     return (
-        <footer className="footer">
-            <button className="footer-btn">
-                Call A Waiter
-            </button>
-            <button className="footer-btn">
-                Thanks for Dining With Us!
-            </button>
-            <button className="footer-btn">
-                Got a Problem? Contact Us
-            </button>
-        </footer>
-    )
+      <footer className="footer">
+        <button className="footer-btn">Call A Waiter</button>
+        <button className="footer-btn">Thanks for Dining With Us!</button>
+        <button className="footer-btn">Got a Problem? Contact Us</button>
+      </footer>
+    );
+  }
 }
-
-export default Footer
