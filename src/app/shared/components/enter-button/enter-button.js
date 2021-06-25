0b1fe61f5d01066b997f9a9926e824753a9b27a7
Jacob Lepler <lepler.jacob@gmail.com>
2021-06-25 13:22:08 -0400

    feat(enter-button) added enter button that links to login

--- src/app/shared/components/enter-button/enter-button.js
+++ src/app/shared/components/enter-button/enter-button.js
@@ -1,2 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export class EnterButton extends Component {
+  render() {
+    return (
+      <Link to="/login">
+        <button
+          className={this.props.buttonClassName}
+          style={{ cursor: "pointer" }}
+        >
+          <img
+            className={this.props.imageClassName}
+            src={this.props.imageSource}
+          />
+        </button>
+      </Link>
+    );
+  }
+}
+
+export default enter - button;
